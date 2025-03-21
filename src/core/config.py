0db03383ae5117a7ea77e00706c72e2019e1d636
Александр <av.sanya@yandex.ru>
2025-03-21 23:47:12 +0500

    Add app lifespan and engine dispose()

--- src/core/config.py
+++ src/core/config.py
@@ -19,8 +19,9 @@ class DatabaseConfig(BaseModel):
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
-        case_sensitive = False, 
-        env_nested_delimiter = "__",
+        case_sensitive=False, 
+        env_nested_delimiter="__",
+        env_prefix="MAIN_CONFIG__"
         )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
--- src/main.py
+++ src/main.py
@@ -1,25 +1,33 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
 import uvicorn
 from sqlalchemy import text
 from core.config import settings
 from api import router as api_router
 from core.database import db_helper
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #app startup
+    yield #back to work cycle
+    #app shutdown
+    await db_helper.dispose()
 
-app = FastAPI()
 
-@app.get("/")
+main_app = FastAPI(lifespan=lifespan)
+
+@main_app.get("/")
 async def root():
     return {"message": "Сервер запущен"}
 
-app.include_router(
+main_app.include_router(
     api_router,
     prefix = settings.api.prefix,
 )
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app", 
+        "main:main_app", 
         host = settings.run.host, 
         port = settings.run.port,
         reload = True)
