de612fe79d727f80f85036554348e3e7ff8ca6be
Александр <av.sanya@yandex.ru>
2025-05-14 02:00:03 +0500

    Minor changes

--- api_gateway/app/routes/users_router.py
+++ api_gateway/app/routes/users_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 import httpx
+from httpx_helper import httpx_helper
 from schemas.users_DTO import NewUser
 from config import settings
 import uuid
@@ -8,37 +9,35 @@ from auth_check import api_key_header
 router = APIRouter(prefix=settings.api.v1.prefix)
 
 @router.post("/public/register", tags=['public'])
-async def register_user(data: NewUser):
-    async with httpx.AsyncClient() as client:
-        try:
-            user = {"name": data.name, "role":data.role.value}
-            response = await client.post(
-                f"{settings.urls.users}/v1/public/register",
-                json=user,
-                timeout=5.0
-            )
-        except httpx.RequestError:
-            raise HTTPException(status_code=502, detail="Сервис Пользователей временно недоступен")
-
+async def register_user(data: NewUser, client: httpx.AsyncClient = Depends(httpx_helper.client_getter)):
+    try:
+        user = {"name": data.name, "role": data.role.value}
+        response = await client.post(
+            f"{settings.urls.users}/v1/public/register",
+            json=user,
+            timeout=5.0
+        )
+    except httpx.RequestError:
+        raise HTTPException(status_code=502, detail="Сервис Пользователей временно недоступен")
     return response.json()
 
 
 @router.delete("/admin/user/{user_id}", tags=['user', 'admin'])
 async def delete_user(
     user_id: uuid.UUID,
-    requester_info: tuple[str, str] = Depends(api_key_header)
-    ):
+    requester_info: tuple[str, str] = Depends(api_key_header),
+    client: httpx.AsyncClient = Depends(httpx_helper.client_getter)
+):
     user_name, role = requester_info
     if role != "ADMIN":
-        raise HTTPException(status_code=403, detail="Method allowed only for admin users")
-    async with httpx.AsyncClient() as client:
-        try:
-            log = {"requester": user_name, "requester_role": role, "userid_to_delete": user_id}
-            print(log)
-            response = await client.delete(
-                f"{settings.urls.users}/v1/admin/user/{user_id}",
-                timeout=5.0
-            )
-        except httpx.RequestError:
-            raise HTTPException(status_code=502, detail="Сервис Пользователей временно недоступен")
+        raise HTTPException(status_code=403, detail="Недостаточно прав для доступа")
+    try:
+        log = {"requester": user_name, "requester_role": role, "userid_to_delete": user_id}
+        print(log)
+        response = await client.delete(
+            f"{settings.urls.users}/v1/admin/user/{user_id}",
+            timeout=5.0
+        )
+    except httpx.RequestError:
+        raise HTTPException(status_code=502, detail="Сервис Пользователей временно недоступен")
     return response.json()
