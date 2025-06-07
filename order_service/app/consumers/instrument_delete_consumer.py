206d29c41166fd60057376a1a174feee3f28a8d3
Александр <av.sanya@yandex.ru>
2025-06-08 02:53:24 +0500

    change to lazy rabbitmq queues

--- order_service/app/consumers/instrument_delete_consumer.py
+++ order_service/app/consumers/instrument_delete_consumer.py
@@ -24,7 +24,12 @@ async def start_consumer():
             channel = await connection.channel()
             await channel.set_qos(prefetch_count=1)
             exchange = await channel.declare_exchange("instrument_deletion_exchange", ExchangeType.FANOUT, durable=True)
-            queue = await channel.declare_queue("instrument_deletion_exchange.ORDER", durable=True)
+            queue = await channel.declare_queue("instrument_deletion_exchange.ORDER",auto_delete=True, durable=True, arguments={
+                "x-queue-mode": "lazy",
+                "x-message-ttl": 60000,
+                "x-max-length": 10000,
+                "x-overflow": "drop-head"
+            })
             await queue.bind(exchange)
             await queue.consume(on_message)
             await asyncio.Future()
